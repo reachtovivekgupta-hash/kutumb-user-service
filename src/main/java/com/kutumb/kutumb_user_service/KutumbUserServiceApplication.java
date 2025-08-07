a3c6d7bafbc7ef90973f8d7cf850955e462e998b
Vivek <Foundvivekgupta@gmail.com>
2025-08-07 09:59:14 +0530

    Initial commit - Kutumb User Service

---
+++ src/main/java/com/kutumb/kutumb_user_service/KutumbUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kutumb.kutumb_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KutumbUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KutumbUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kutumb/kutumb_user_service/KutumbUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutumb.kutumb_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutumbUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
